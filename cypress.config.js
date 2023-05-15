951aa4d79525401e3801cd87b6ba8b88e262dbd6
Andreas Jonsson <andreas@andreasjonsson.de>
2023-05-15 22:13:00 +0200

    Add cypress env vars via dotenv
    Including the base url

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
+require("dotenv").config();
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+      config.baseUrl = process.env.CYPRESS_BASE_URL || config.baseUrl;
+      config.env = {
+        ...process.env,
+        ...config.env
+      };
+      return config;
+    }
+  }
 });
