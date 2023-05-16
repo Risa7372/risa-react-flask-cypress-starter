e6c5c3a76c277119beca520f95803c99498abd7c
Andreas Jonsson <andreas@andreasjonsson.de>
2023-05-16 16:35:57 +0200

    set cypress default timeout to 10000

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 10000,
   e2e: {
     setupNodeEvents(on, config) {
       config.baseUrl = process.env.CYPRESS_BASE_URL || config.baseUrl;
